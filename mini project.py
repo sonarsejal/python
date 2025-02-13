991713a309aad6f94b9c521ad8551f6e3d5dfa8a
Sejal Sonar <sejalsonar10@gmail.com>
2025-02-13 20:04:59 +0530

    Create mini project.py

---
+++ mini project.py
@@ -0,0 +1,17 @@
+# Calculator created using python
+first = int(input("enter first number:"))
+operator = input("enter operator (+,-,*,/,%):")
+second = int(input("enter second number:"))
+
+if operator == "+":
+    print(first + second)
+elif operator == "-":
+    print(first - second)
+elif operator == "*":
+    print(first * second)
+elif operator == "/":
+    print(first / second)
+elif operator == "%":
+    print(first % second)
+else:
+    print("Invalid operator")
